e3c67503ddb451faf663d30fc1fcaf6fd94c3550
Lucky Tripura <mt1230144@maths.iitd.ac.in>
2024-10-12 21:15:28 +0530

    Add files via upload

---
+++ crewmate.py
@@ -0,0 +1,10 @@
+
+class CrewMate:
+    def __init__(self):
+        self.total_treasure = 0
+        self.treasure = []
+        self.scheduled_load = 0
+        self.id = None
+        self.pointers = []
+  
+        
