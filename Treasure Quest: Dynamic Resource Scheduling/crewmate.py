eef8cf35cda38666bb84b63588ac241f0707b68e
Lucky Tripura <mt1230144@maths.iitd.ac.in>
2024-10-13 23:08:20 +0530

    Update crewmate.py

--- Treasure Quest: Dynamic Resource Scheduling/crewmate.py
+++ Treasure Quest: Dynamic Resource Scheduling/crewmate.py
@@ -1,1 +1,9 @@
-
+class CrewMate:
+    def __init__(self):
+        self.total_treasure = 0
+        self.treasure = []
+        self.scheduled_load = 0
+        self.id = None
+        self.pointers = []
+  
+        
